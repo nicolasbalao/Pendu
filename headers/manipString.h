84c3ccc8b83a717224bfd62a7fb9dc7023b9c7a2
nicola balao <nicolasbalao2@gmail.com>
2021-09-15 17:23:23 +0200

    Version 1

---
+++ headers/manipString.h
@@ -0,0 +1,5 @@
+void printTableau(char *tableau, int sizeTableau);
+void initInputWord(char *inputWord, int sizeTableau);
+int testWin(char *tableau1, char *tableau2, int sizeTableau);
+void testLetterInWord(char letter, char *mysteryWord, char *InputWord, int sizeTableau, int *coup);
+char lireCaractere();
---
+++ source/manipString.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+void printTableau(char *tableau, int sizeTableau)
+{
+    for (int i = 0; i < sizeTableau; i++)
+    {
+        printf("%c", tableau[i]);
+    }
+    printf("\n");
+}
+
+void initInputWord(char *inputWord, int sizeTableau)
+{
+    for (int i = 0; i < sizeTableau; i++)
+    {
+        inputWord[i] = '*';
+    }
+}
+
+int testWin(char *tableau1, char *tableau2, int sizeTableau)
+{
+    for (int i = 0; i < sizeTableau; i++)
+    {
+        if (tableau1[i] != tableau2[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void testLetterInWord(char letter, char *mysteryWord, char *InputWord, int sizeTableau, int *coup)
+{
+    int winRound = 0;
+    for (int i = 0; i < sizeTableau; i++)
+    {
+        if (letter == mysteryWord[i])
+        {
+            InputWord[i] = letter;
+            winRound = 1;
+        }
+    }
+    if (winRound == 0)
+    {
+        *coup = *coup - 1;
+    }
+}
+
+char lireCaractere()
+{
+    char caractere = getchar();
+    while (getchar() != '\n') //lit tous les caractère jusqu'a \n
+        ;
+    return caractere;
+}
